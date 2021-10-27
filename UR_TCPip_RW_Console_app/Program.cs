1caceffcbf8c63afc77aa675982a1f679d794909
Roman Parak <59449320+rparak@users.noreply.github.com>
2021-10-27 17:13:32 +0100

    Update Program.cs

--- UR_TCPip_RW_Console_app/Program.cs
+++ UR_TCPip_RW_Console_app/Program.cs
@@ -68,7 +68,7 @@ namespace UR_TCPip_RW_Console_app
         //      Orientation {Euler Angles} (rad):
         public static double[] C_Orientation = new double[3] { 0.0, 3.11, 0.0 };
         // Move Parameters: Velocity, Acceleration
-        public static string velocity     = "1.0";
+        public static string velocity = "1.0";
         public static string acceleration = "1.0";
     }
 
@@ -80,11 +80,11 @@ namespace UR_TCPip_RW_Console_app
             //  Read Data:
             UR_Stream_Data.ip_address = "192.168.230.133";
             //  Communication speed: CB-Series 125 Hz, E-Series 500 Hz
-            UR_Stream_Data.time_step = ((1/125) * 1000);
+            UR_Stream_Data.time_step = ((1 / 125) * 1000);
             //  Write Data:
             UR_Control_Data.ip_address = "192.168.230.133";
             //  Communication speed: CB-Series 125 Hz, E-Series 500 Hz
-            UR_Control_Data.time_step = ((1/125) * 1000);
+            UR_Control_Data.time_step = ((1 / 125) * 1000);
 
             // Start Stream {Universal Robots TCP/IP}
             UR_Stream ur_stream_robot = new UR_Stream();
@@ -123,10 +123,11 @@ namespace UR_TCPip_RW_Console_app
     class UR_Stream
     {
         // Initialization of variables
-        private Thread robot_thread = null;
-        private Socket tcp_server = null;
+        private Thread robot_thread      = null;
+        private TcpClient tcp_client     = new TcpClient();
+        private NetworkStream network_stream = null;
         private bool exit_thread = false;
-        private byte[] packet = new byte[1108];
+        private byte[] packet = new byte[2048];
 
         // Offset:
         //  Size of first packet in bytes (Integer)
@@ -136,50 +137,51 @@ namespace UR_TCPip_RW_Console_app
 
         public void UR_Stream_Thread()
         {
-            // Create socket for TCP/IP server
-            tcp_server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            // Initialization TCP/IP Communication
-            IPAddress ip_address = IPAddress.Parse(UR_Stream_Data.ip_address);
-            IPEndPoint end_point = new IPEndPoint(ip_address, UR_Stream_Data.port_number);
-
-            if (tcp_server.Connected == false)
+            try
             {
-                // connect to controller -> if the controller is disconnected
-                tcp_server.Connect(end_point);
-            }
-
-            while (exit_thread == false)
-            {
-                // Get the data from the robot
-                if (tcp_server.Receive(packet) == packet.Length)
+                if (tcp_client.Connected == false)
                 {
-                    // Reverses the order of elements in a one-dimensional array or part of an array.
-                    Array.Reverse(packet);
-
-                    // Note:
-                    //  For more information on values 32... 37, etc., see the UR Client Interface document.
-                
-                    // Read Joint Values in radians
-                    UR_Stream_Data.J_Orientation[0] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (32 * offset));
-                    UR_Stream_Data.J_Orientation[1] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (33 * offset));
-                    UR_Stream_Data.J_Orientation[2] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (34 * offset));
-                    UR_Stream_Data.J_Orientation[3] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (35 * offset));
-                    UR_Stream_Data.J_Orientation[4] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (36 * offset));
-                    UR_Stream_Data.J_Orientation[5] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (37 * offset));
-
-                    // Read Cartesian (Positon) Values in metres
-                    UR_Stream_Data.C_Position[0] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (56 * offset));
-                    UR_Stream_Data.C_Position[1] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (57 * offset));
-                    UR_Stream_Data.C_Position[2] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (58 * offset));
-                    // Read Cartesian (Orientation) Values in metres 
-                    UR_Stream_Data.C_Orientation[0] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (59 * offset));
-                    UR_Stream_Data.C_Orientation[1] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (60 * offset));
-                    UR_Stream_Data.C_Orientation[2] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (61 * offset));
-
-                    //Console.WriteLine(UR_Stream_Data.C_Orientation[0]);
+                    // Connect to controller -> if the controller is disconnected
+                    tcp_client.Connect(UR_Stream_Data.ip_address, UR_Stream_Data.port_number);
                 }
-                Thread.Sleep(UR_Stream_Data.time_step);
+
+                // Initialization TCP/IP Communication (Stream)
+                network_stream = tcp_client.GetStream();
+
+                while (exit_thread == false)
+                {
+                    // Get the data from the robot
+                    if (network_stream.Read(packet, 0, packet.Length) > 0)
+                    {
+                        // Reverses the order of elements in a one-dimensional array or part of an array.
+                        Array.Reverse(packet);
+
+                        // Note:
+                        //  For more information on values 32... 37, etc., see the UR Client Interface document.
+
+                        // Read Joint Values in radians
+                        UR_Stream_Data.J_Orientation[0] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (32 * offset));
+                        UR_Stream_Data.J_Orientation[1] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (33 * offset));
+                        UR_Stream_Data.J_Orientation[2] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (34 * offset));
+                        UR_Stream_Data.J_Orientation[3] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (35 * offset));
+                        UR_Stream_Data.J_Orientation[4] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (36 * offset));
+                        UR_Stream_Data.J_Orientation[5] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (37 * offset));
+
+                        // Read Cartesian (Positon) Values in metres
+                        UR_Stream_Data.C_Position[0] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (56 * offset));
+                        UR_Stream_Data.C_Position[1] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (57 * offset));
+                        UR_Stream_Data.C_Position[2] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (58 * offset));
+                        // Read Cartesian (Orientation) Values in metres 
+                        UR_Stream_Data.C_Orientation[0] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (59 * offset));
+                        UR_Stream_Data.C_Orientation[1] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (60 * offset));
+                        UR_Stream_Data.C_Orientation[2] = BitConverter.ToDouble(packet, packet.Length - first_packet_size - (61 * offset));
+                    }
+                    Thread.Sleep(UR_Stream_Data.time_step);
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
             }
         }
 
@@ -196,7 +198,8 @@ namespace UR_TCPip_RW_Console_app
             if (robot_thread == null && robot_thread.IsAlive == true)
             {
                 // Disconnect communication
-                tcp_server.Disconnect(true);
+                network_stream.Dispose();
+                tcp_client.Close();
                 Thread.Sleep(1000);
             }
         }
@@ -206,58 +209,75 @@ namespace UR_TCPip_RW_Console_app
     {
         // Initialization of variables
         private Thread robot_thread = null;
-        private Socket tcp_server = null;
+        private TcpClient tcp_client = new TcpClient();
+        private NetworkStream network_stream = null;
         private bool exit_thread = false;
         private UTF8Encoding utf8 = new UTF8Encoding();
-        byte[] cmd;
+        byte[] buffer_cmd;
 
         public void UR_Control_Thread()
         {
-            // Create socket for TCP/IP server
-            tcp_server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            // Initialization TCP/IP Communication
-            IPAddress ip_address = IPAddress.Parse(UR_Control_Data.ip_address);
-            IPEndPoint end_point = new IPEndPoint(ip_address, UR_Control_Data.port_number);
-
-            if (tcp_server.Connected == false)
+            if (tcp_client.Connected == false)
             {
-                // connect to controller -> if the controller is disconnected
-                tcp_server.Connect(end_point);
+                // Connect to controller -> if the controller is disconnected
+                tcp_client.Connect(UR_Control_Data.ip_address, UR_Control_Data.port_number);
             }
+
+            // Initialization TCP/IP Communication (Stream)
+            network_stream = tcp_client.GetStream();
 
             while (exit_thread == false)
             {
+                // https://www.universal-robots.com/articles/ur/interface-communication/tcpip-socket-communication-via-urscript/
+
                 // Note:
                 //  For more information about commands, see the URScript Programming Language document 
-                
-                // Instruction 1 (Home Position): Joint Command
+
+                // Instruction 1 (Home Position): Joint Input Command, Move Joint Interpolation
                 //  Get Bytes from String
-                cmd = utf8.GetBytes("movej([" + UR_Control_Data.J_Orientation[0].ToString() + "," + UR_Control_Data.J_Orientation[1].ToString() + "," + UR_Control_Data.J_Orientation[2].ToString() + "," 
-                                              + UR_Control_Data.J_Orientation[3].ToString() + "," + UR_Control_Data.J_Orientation[4].ToString() + "," + UR_Control_Data.J_Orientation[5].ToString() + "]," 
-                                              + "a=" + UR_Control_Data.acceleration + ", v=" + UR_Control_Data.velocity + ")" + "\n");
+                buffer_cmd = utf8.GetBytes("movej([" + UR_Control_Data.J_Orientation[0].ToString() + "," + UR_Control_Data.J_Orientation[1].ToString() + "," + UR_Control_Data.J_Orientation[2].ToString() + ","
+                                                     + UR_Control_Data.J_Orientation[3].ToString() + "," + UR_Control_Data.J_Orientation[4].ToString() + "," + UR_Control_Data.J_Orientation[5].ToString() + "],"
+                                                     + "a=" + UR_Control_Data.acceleration + ", v=" + UR_Control_Data.velocity + ")" + "\n");
                 //  Send command to the robot
-                tcp_server.Send(cmd);
+                network_stream.Write(buffer_cmd);
                 //  Wait Time (5 seconds)
                 Thread.Sleep(5000);
 
-                // Instruction 2 (Position Z - 100 mm): Cartesian Command
+                // Instruction 2 (Multiple Positions): Cartesian Input Command, Move Linear Interpolation
                 //  Get Bytes from String
-                cmd = utf8.GetBytes("movej(p[" + UR_Control_Data.C_Position[0].ToString() + "," + UR_Control_Data.C_Position[1].ToString() + "," + (UR_Control_Data.C_Position[2] - 0.1).ToString() + ","
-                                               + UR_Control_Data.C_Orientation[0].ToString() + "," + UR_Control_Data.C_Orientation[1].ToString() + "," + UR_Control_Data.C_Orientation[2].ToString() + "],"
-                                               + "a=" + UR_Control_Data.acceleration + ", v=" + UR_Control_Data.velocity + ")" + "\n");
+                buffer_cmd = utf8.GetBytes("[movel(p[" + UR_Control_Data.C_Position[0].ToString() + "," + UR_Control_Data.C_Position[1].ToString() + "," + (UR_Control_Data.C_Position[2] - 0.1).ToString() + ","
+                                                       + UR_Control_Data.C_Orientation[0].ToString() + "," + UR_Control_Data.C_Orientation[1].ToString() + "," + UR_Control_Data.C_Orientation[2].ToString() + "],"
+                                                       + "a=" + UR_Control_Data.acceleration + ", v=" + UR_Control_Data.velocity + ")," +
+                                           "movel(p[" + (UR_Control_Data.C_Position[0] - 0.1).ToString() + ", " + UR_Control_Data.C_Position[1].ToString() + ", " + (UR_Control_Data.C_Position[2] - 0.1).ToString() + ", "
+                                                       + UR_Control_Data.C_Orientation[0].ToString() + "," + UR_Control_Data.C_Orientation[1].ToString() + "," + UR_Control_Data.C_Orientation[2].ToString() + "],"
+                                                       + "a=" + UR_Control_Data.acceleration + ", v=" + UR_Control_Data.velocity + ")," +
+                                           "movel(p[" + (UR_Control_Data.C_Position[0] - 0.1).ToString() + ", " + UR_Control_Data.C_Position[1].ToString() + ", " + UR_Control_Data.C_Position[2].ToString() + ", "
+                                                       + UR_Control_Data.C_Orientation[0].ToString() + "," + UR_Control_Data.C_Orientation[1].ToString() + "," + UR_Control_Data.C_Orientation[2].ToString() + "],"
+                                                       + "a=" + UR_Control_Data.acceleration + ", v=" + UR_Control_Data.velocity + ")," +
+                                           "movel(p[" + UR_Control_Data.C_Position[0].ToString() + ", " + UR_Control_Data.C_Position[1].ToString() + ", " + (UR_Control_Data.C_Position[2]).ToString() + ", "
+                                                       + UR_Control_Data.C_Orientation[0].ToString() + "," + UR_Control_Data.C_Orientation[1].ToString() + "," + UR_Control_Data.C_Orientation[2].ToString() + "],"
+                                                       + "a=" + UR_Control_Data.acceleration + ", v=" + UR_Control_Data.velocity + ")]" + "\n");
                 //  Send command to the robot
-                tcp_server.Send(cmd);
+                network_stream.Write(buffer_cmd);
                 //  Wait Time (5 seconds)
                 Thread.Sleep(5000);
 
-                // Instruction 3 (Home Position): Joint Command
+                // Instruction 3 (Multiple Positions): Cartesian Input Command, Move Joint Interpolation
                 //  Get Bytes from String
-                cmd = utf8.GetBytes("movej([" + UR_Control_Data.J_Orientation[0].ToString() + "," + UR_Control_Data.J_Orientation[1].ToString() + "," + UR_Control_Data.J_Orientation[2].ToString() + ","
-                                              + UR_Control_Data.J_Orientation[3].ToString() + "," + UR_Control_Data.J_Orientation[4].ToString() + "," + UR_Control_Data.J_Orientation[5].ToString() + "],"
-                                              + "a=" + UR_Control_Data.acceleration + ", v=" + UR_Control_Data.velocity + ")" + "\n");
+                buffer_cmd = utf8.GetBytes("[movej(p[" + UR_Control_Data.C_Position[0].ToString() + "," + UR_Control_Data.C_Position[1].ToString() + "," + (UR_Control_Data.C_Position[2] - 0.1).ToString() + ","
+                                                       + UR_Control_Data.C_Orientation[0].ToString() + "," + UR_Control_Data.C_Orientation[1].ToString() + "," + UR_Control_Data.C_Orientation[2].ToString() + "],"
+                                                       + "a=" + UR_Control_Data.acceleration + ", v=" + UR_Control_Data.velocity + ")," +
+                                           "movej(p[" + (UR_Control_Data.C_Position[0] - 0.1).ToString() + ", " + UR_Control_Data.C_Position[1].ToString() + ", " + (UR_Control_Data.C_Position[2] - 0.1).ToString() + ", "
+                                                       + UR_Control_Data.C_Orientation[0].ToString() + "," + UR_Control_Data.C_Orientation[1].ToString() + "," + UR_Control_Data.C_Orientation[2].ToString() + "],"
+                                                       + "a=" + UR_Control_Data.acceleration + ", v=" + UR_Control_Data.velocity + ")," +
+                                           "movej(p[" + (UR_Control_Data.C_Position[0] - 0.1).ToString() + ", " + UR_Control_Data.C_Position[1].ToString() + ", " + UR_Control_Data.C_Position[2].ToString() + ", "
+                                                       + UR_Control_Data.C_Orientation[0].ToString() + "," + UR_Control_Data.C_Orientation[1].ToString() + "," + UR_Control_Data.C_Orientation[2].ToString() + "],"
+                                                       + "a=" + UR_Control_Data.acceleration + ", v=" + UR_Control_Data.velocity + ")," +
+                                           "movej(p[" + UR_Control_Data.C_Position[0].ToString() + ", " + UR_Control_Data.C_Position[1].ToString() + ", " + (UR_Control_Data.C_Position[2]).ToString() + ", "
+                                                       + UR_Control_Data.C_Orientation[0].ToString() + "," + UR_Control_Data.C_Orientation[1].ToString() + "," + UR_Control_Data.C_Orientation[2].ToString() + "],"
+                                                       + "a=" + UR_Control_Data.acceleration + ", v=" + UR_Control_Data.velocity + ")]" + "\n");
                 //  Send command to the robot
-                tcp_server.Send(cmd);
+                network_stream.Write(buffer_cmd);
                 //  Wait Time (5 seconds)
                 Thread.Sleep(5000);
             }
@@ -276,7 +296,8 @@ namespace UR_TCPip_RW_Console_app
             if (robot_thread == null && robot_thread.IsAlive == true)
             {
                 // Disconnect communication
-                tcp_server.Disconnect(true);
+                network_stream.Dispose();
+                tcp_client.Close();
                 Thread.Sleep(1000);
             }
         }
